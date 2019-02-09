384953f399ca8dee9cc9443baa67cf6fbc482cdc
zcc <listenzcc@mail.bnu.edu.cn>
2019-02-09 14:35:57 +0800

    Game begins.

--- demo_33game.py
+++ demo_33game.py
@@ -1,4 +1,5 @@
 # code: utf-8
+import numpy as np
 
 shape = {0: ' ', 1: 'x', -1: 'o'}
 
@@ -47,16 +48,19 @@ def check_if_threelink(state, who, continues=continues):
 result_dict = dict()
 
 
-def check_state(state, who):
+def check_state(state, who, result_dict=result_dict):
     # pnt_board(state)
 
     if check_if_threelink(state, who):
+        result_dict[str(state)] = [True]
         return True
 
     if any(check_if_threelink(p, -who) for p in possibles(state, -who)):
+        result_dict[str(state)] = [False]
         return False
 
     if possibles(state, -who) == []:
+        result_dict[str(state)] = [False]
         return False
 
     ll = list(any(check_state(p1, who) for p1 in possibles(p0, who))
@@ -68,16 +72,39 @@ def check_state(state, who):
     return True
 
 
-state = [0, 0, 0,
+def get_scores(state, who):
+    scores = dict()
+    for p in possibles(state, who):
+        check_state(p, who)
+        res = result_dict[str(p)]
+        scores[str(p)] = [sum(10+np.random.random()
+                              for _ in range(sum(res))), res]
+    return scores
+
+
+def diff_idx(state, newstate):
+    assert(not state == newstate)
+    for j in range(9):
+        if not state[j] == newstate[j]:
+            return j
+
+
+def get_idx_bestmove(state, who):
+    scores = get_scores(state, who)
+    ms = []
+    for it in scores.items():
+        print(it[0], it[1])
+        if not any(not e for e in it[1][1]):
+            ms = it[0]
+            break
+    if ms == []:
+        ms = max(scores, key=scores.get)
+    return diff_idx(state, list(int(e) for e in ms[1:-1].split(', ')))
+
+
+state = [1, -1, 0,
          0, 0, 0,
          0, 0, 0]
-
-for p in possibles(state, 1):
-    check_state(p, 1)
-
-for p in possibles(state, 1):
-    res = result_dict[str(p)]
-    print(p, res, sum(res))
 
 
 class Game_3x3:
@@ -89,14 +116,35 @@ class Game_3x3:
         self.state = list(0 for _ in range(9))
         self.who = 1
 
+    def pnt(self):
+        pnt_board(self.state)
+
+    def get_input(self):
+        idx = -1
+        while idx not in range(9):
+            idx = int(input(self.shape[self.who]+': '))
+        return idx
+
+    def finish(self):
+        if check_if_threelink(self.state, self.who):
+            print(self.shape[self.who], 'wins')
+            return True
+        if sum(1 for j in self.state if j == 0) == 0:
+            print('even')
+            return True
+        return False
+
     def go(self):
         self.refresh()
         while True:
-            pnt_board(self.state)
-            idx = -1
-            while idx not in range(9):
-                idx = int(input(self.shape[self.who]+': '))
+            if self.who == 1:
+                idx = get_idx_bestmove(self.state, 1)
+            else:
+                idx = self.get_input()
             self.state[idx] = self.who
+            self.pnt()
+            if self.finish():
+                break
             self.who *= -1
 
 
